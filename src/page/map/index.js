e9fa069e425d397427774706c2a30d307781e51c
HytonightYX <whosiyuan@qq.com>
2019-12-18 22:10:21 +0800

    :tada: 加入高德地图

--- src/page/map/index.js
+++ src/page/map/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import './style.less'
 
-class Map extends React.Component {
+class MapClass extends React.Component {
+
+	state = {
+		loading: true
+	}
+
+	componentDidMount() {
+		var map = new AMap.Map('container', {
+			pitch: 50,
+			zoom: 11
+		})
+
+		AMap.plugin('AMap.CitySearch', function () {
+			var citySearch = new AMap.CitySearch()
+			citySearch.getLocalCity(function (status, result) {
+				if (status === 'complete' && result.info === 'OK') {
+					console.log(result)
+				}
+			})
+		})
+	}
 
 	render() {
 		return (
 			<div className="g-map">
-				Map
+				<div id="container" className="map-wrap"/>
 			</div>
 		)
 	}
+
 }
 
-export default Map
+export default MapClass
